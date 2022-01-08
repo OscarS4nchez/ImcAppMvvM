9c8064fa8e977982948af2dba3e5179659fe1237
OscarS4nchez <17470008@campeche.tecnm.mx>
2022-01-08 17:06:23 -0600

    indice de masa corporal mvvm

---
+++ ImcApp/ImcApp/Models/CalculadoraImc.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ImcApp.Models
+{
+    
+
+
+    public class CalculadoraImc
+    { 
+
+
+
+
+
+        public static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura)
+        
+            
+                => peso / (estatura * estatura);
+         
+
+        public enum EstadoNutricional
+        {
+            PesoBajo,
+            PesoNormal,
+            SobrePeso,
+            Obesidad,
+            ObesidadExtrema
+        }
+
+        public static EstadoNutricional SituacionNutricional(decimal imc)
+        {
+           
+                if (imc < 18.5M)
+                {
+                    return EstadoNutricional.PesoBajo;
+                }
+                else if (imc < 25.0M)
+                {
+                    return EstadoNutricional.PesoNormal;
+                }
+                else if (imc < 30.0M)
+                {
+                    return EstadoNutricional.SobrePeso;
+                }
+                else if (imc < 40.0M)
+                {
+                    return EstadoNutricional.Obesidad;
+
+                }
+                else
+                {
+                    return EstadoNutricional.ObesidadExtrema;
+                }
+            }
+        }
+    }
+
+
+
+
---
+++ ImcApp/ImcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using Xamarin.Forms;
+using ImcApp.Models;
+using ImcApp.ViewModels;
+
+
+namespace ImcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+
+
+
+        }
+    }
+}
